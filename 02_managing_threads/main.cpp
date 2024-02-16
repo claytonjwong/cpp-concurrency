a5d897cae780b42f1f5dc3532224250dd85044f0
claytonjwong <claytonjwong@gmail.com>
2024-02-15 18:25:43 -0700

    Chapter 02: Managing Threads -- Listing 2.6 scoped_thread and example usage

--- 02_managing_threads/main.cpp
+++ 02_managing_threads/main.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
+#include <sstream>
 #include <thread>
 
 using namespace std;
 
 void do_work(int n) {
+    auto f = [](auto i) {
+        ostringstream os;
+        os << "do_work(" << i << ")" << endl;
+        return os.str();
+    };
     for (auto i{ 0 }; i < n; ++i)
-        cout << "do_work(" << i << ")" << endl;
+        cout << f(i), flush(cout);
 }
 
 class thread_guard {
@@ -22,9 +28,31 @@ public:
     thread_guard& operator=(const thread_guard&&) = delete;
 };
 
+class scoped_thread {
+    thread t;
+public:
+    explicit scoped_thread(thread t_) : t{ std::move(t_) } {
+        if (!t.joinable())
+            throw logic_error("No thread!");
+    }
+    ~scoped_thread() {
+        t.join();
+    }
+    scoped_thread(const scoped_thread&) = delete;
+    scoped_thread(const scoped_thread&&) = delete;
+    scoped_thread& operator=(const scoped_thread&) = delete;
+    scoped_thread& operator=(const scoped_thread&&) = delete;
+};
+
 int main() {
     int N = 10;
+
+    // Listing 2.3: Using RAII to wait for a thread to complete
     thread t(do_work, N);
     thread_guard g(t);
+
+    // Listing 2.6: scoped_thread and example usage
+    scoped_thread s(thread(do_work, N));
+
     return 0;
 }
