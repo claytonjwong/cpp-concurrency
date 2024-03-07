60c8520393c7d55c3ef2dcf41867f879b312cf25
claytonjwong <claytonjwong@gmail.com>
2024-03-06 20:14:10 -0700

    parallel quick sort

--- 04_synchronization/quicksort/main.cpp
+++ 04_synchronization/quicksort/main.cpp
@@ -1,14 +1,15 @@
 #include <algorithm>
+#include <future>
 #include <iostream>
 #include <list>
 #include <numeric>
 #include <random>
+#include <vector>
 
 template<typename T>
 std::list<T> sequential_quick_sort(std::list<T> A) {
-    if (A.empty()) {
+    if (A.size() < 2)
         return A;
-    }
     std::list<T> res;
     res.splice(res.begin(), A, A.begin());
     auto& pivot = *res.begin();
@@ -20,9 +21,24 @@ std::list<T> sequential_quick_sort(std::list<T> A) {
     return res;
 }
 
+template<typename T>
+std::list<T> parallel_quick_sort(std::list<T> A) {
+    if (A.size() < 2)
+        return A;
+    std::list<T> res;
+    res.splice(res.begin(), A, A.begin());
+    auto& pivot = *res.begin();
+    auto half = std::partition(A.begin(), A.end(), [=](auto x) { return x < pivot; });
+    std::list<T> lo; lo.splice(lo.end(), A, A.begin(), half);
+    std::list<T> hi; hi.splice(hi.end(), A, half, A.end());
+    res.splice(res.begin(), std::async(parallel_quick_sort<T>, std::move(lo)).get());
+    res.splice(res.end(), std::async(parallel_quick_sort<T>, std::move(hi)).get());
+    return res;
+}
+
 int main() {
     std::list<int> A{ 5, 7, 3, 4, 1, 9, 2, 8, 10, 6 };
-    auto S = sequential_quick_sort(A);
+    auto S = parallel_quick_sort(A);
     std::copy(A.begin(), A.end(), std::ostream_iterator<int>(std::cout, " ")), std::cout << std::endl;
     std::copy(S.begin(), S.end(), std::ostream_iterator<int>(std::cout, " ")), std::cout << std::endl;
     // 5 7 3 4 1 9 2 8 10 6
