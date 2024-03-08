d572f3e25e115c78c73a51160f48bf50fac33f8a
claytonjwong <claytonjwong@gmail.com>
2024-03-07 19:12:56 -0700

    boost .then working example -- cleanup ephemeral variables and instead use .then directly

--- 04_synchronization/promises/README.md
+++ 04_synchronization/promises/README.md
@@ -5,6 +5,20 @@ cmake . && cmake --build . && ./cmake-bin/main
 
 # Output
 ```
+➜  promises git:(main) ✗ cmake . && cmake --build . && ./cmake-bin/main
+C_COMPILER: /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/cc
+CXX_COMPILER: /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/c++
+Boost: 1.84
+-- Configuring done
+-- Generating done
+-- Build files have been written to: /Users/claytonjwong/sandbox/cpp-concurrency/04_synchronization/promises
+Consolidate compiler generated dependencies of target main
+[ 50%] Building CXX object CMakeFiles/main.dir/main.cpp.o
+[100%] Linking CXX executable cmake-bin/main
+[100%] Built target main
 A: 1 2 3 4 5 6 7 8 9 10
-sum: 55
+and_then_1...       55
+and_then_2...      155
+and_then_3...      255
+no_more_and_then!  355
 ```
--- 04_synchronization/promises/main.cpp
+++ 04_synchronization/promises/main.cpp
@@ -27,33 +27,33 @@ int main() {
     boost::future<VI> gen_future = gen_promise.get_future();
     boost::thread t1{ gen, std::ref(gen_promise) };
 
-    auto and_then_0 = gen_future.then([](boost::future<VI> gen_future) {
+    auto and_then = gen_future.then([](boost::future<VI> gen_future) {  // and_then 0
         auto A = gen_future.get();
         std::cout << "A: ", std::copy(A.begin(), A.end(), std::ostream_iterator<int>(std::cout, " ")), std::cout << std::endl;
         return A;
     });
 
     boost::promise<int> sum_promise;
-    boost::future<int> sum_future_0 = sum_promise.get_future();
-    boost::thread t2{ sum, and_then_0.get(), std::ref(sum_promise) };
-    auto and_then_1 = sum_future_0.then([](boost::future<int> sum_future_1) {
-        auto total = sum_future_1.get();
+    boost::future<int> sum_future = sum_promise.get_future();
+    boost::thread t2{ sum, and_then.get(), std::ref(sum_promise) };
+
+    sum_future.then([](boost::future<int> sum_future) {  // and_then_1
+        auto total = sum_future.get();
         std::cout << "and_then_1...       " << total << std::endl;
         return total;
-    });
-    auto and_then_2 = and_then_1.then([](boost::future<int> sum_future_2) {
-        auto total = sum_future_2.get() + 100;
+    }).then([](boost::future<int> sum_future) {  // and_then_2
+        auto total = sum_future.get() + 100;
         std::cout << "and_then_2...      " << total << std::endl;
         return total;
-    });
-    auto and_then_3 = and_then_2.then([](boost::future<int> sum_future_3) {
-        auto total = sum_future_3.get() + 100;
+    }).then([](boost::future<int> sum_future) {  // and_then_3
+        auto total = sum_future.get() + 100;
         std::cout << "and_then_3...      " << total << std::endl;
         return total;
-    });
-
-    auto no_more_and_then = and_then_3.get() + 100;
-    std::cout << "no_more_and_then!  " << no_more_and_then << std::endl;
+    }).then([](boost::future<int> sum_future) {  // no_more_and_then
+        auto total = sum_future.get() + 100;
+        std::cout << "no_more_and_then!  " << total << std::endl;
+        return total;
+    }).get();
 
     t1.join();
     t2.join();
