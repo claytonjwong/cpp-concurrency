2c86b5c5a31f8e4230314b332e875ed89010ad19
claytonjwong <claytonjwong@gmail.com>
2024-03-08 08:00:20 -0700

    minor edits: use 'auto' for conciseness

--- 04_synchronization/promises/main.cpp
+++ 04_synchronization/promises/main.cpp
@@ -20,32 +20,32 @@ void gen(boost::promise<VI>& p) {
 
 int main() {
     boost::promise<VI> gen_promise;
-    boost::future<VI> gen_future = gen_promise.get_future();
+    auto gen_future = gen_promise.get_future();
     boost::thread thread{ gen, std::ref(gen_promise) };
 
-    auto and_then = gen_future.then([](boost::future<VI> gen_future) {  // and_then 0
+    auto and_then = gen_future.then([](auto gen_future) {  // and_then 0
         auto A = gen_future.get();
         std::cout << "A: ", std::copy(A.begin(), A.end(), std::ostream_iterator<int>(std::cout, " ")), std::cout << std::endl;
         return std::accumulate(A.begin(), A.end(), 0);
-    }).then([](boost::future<int> sum_future) {  // and_then_1
+    }).then([](auto sum_future) {  // and_then_1
         auto total = sum_future.get();
         std::cout << "and_then_1...       " << total << std::endl;
         return total;
-    }).then([](boost::future<int> sum_future) {  // and_then_2
+    }).then([](auto sum_future) {  // and_then_2
         auto total = sum_future.get() + 100;
         std::cout << "and_then_2...      " << total << std::endl;
         return total;
-    }).then([](boost::future<int> sum_future) {  // and_then_3
+    }).then([](auto sum_future) {  // and_then_3
         auto total = sum_future.get() + 100;
         std::cout << "and_then_3...      " << total << std::endl;
         return total;
-    }).then([](boost::future<int> sum_future) {  // no_more_and_then
+    }).then([](auto sum_future) {  // no_more_and_then
         auto total = sum_future.get() + 100;
         std::cout << "no_more_and_then!  " << total << std::endl;
         return total;
     }).get();
 
-    thread.join();
+    std::cout << "ok byebye" << std::endl;
 
     return 0;
 }
